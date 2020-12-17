8f8494de142329f7e4f93a086bc2c2b9b4d3b71b
Ben Stoller <stollersystemsound@gmail.com>
2020-12-17 10:52:35 -0800

    test out delay and filter sliders - working

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -8,8 +8,10 @@ class Synth extends Component {
     BPM: 90
   }
 
-  volume = new Tone.Volume(0)
-  synth = new Tone.Synth().chain(this.volume, Tone.Destination);
+  volume = new Tone.Volume(-20);
+  delay = new Tone.FeedbackDelay(.5, .5);  
+  filter = new Tone.Filter(7500, 'lowpass', -24)
+  synth = new Tone.Synth().chain(this.volume, this.delay, this.filter, Tone.Destination);
 
   
 
@@ -23,6 +25,7 @@ class Synth extends Component {
   )
 
   handleSequenceStart = () => {
+    Tone.start();
     this.sequence.start(0)
     Tone.Transport.toggle() 
     Tone.Transport.bpm.value = this.state.BPM  
@@ -33,12 +36,37 @@ class Synth extends Component {
     Tone.Transport.toggle()
   }
 
+  handleDelayLevel = () => {
+
+  }
+
+  componentDidMount() {
+
+    const delay = this.delay; 
+    const filter = this.filter
+    delay.wet.value = 0;
+
+    var delaySlide = document.getElementById('delayLevel');
+    delaySlide.addEventListener("input", function() {  
+      console.log(this.value)
+      delay.wet.value = this.value/100;     
+    });  
+
+    var filterSlide = document.getElementById('filterCutoff');
+    filterSlide.addEventListener("input", function() {     
+      filter.frequency.value = this.value*100;     
+    });
+
+  }
+
   render() {
     return(
       <React.Fragment>
         <p>synth test</p>
         <button onClick={this.handleSequenceStart}>start</button>
         <button onClick={this.handleSequenceStop}>stop</button>
+        <p>DLY LVL<input type="range" min="0" max="100" defaultValue="0" className="slider" id="delayLevel"/></p> 
+        <p>FILTER<input type="range" min="0" max="100" defaultValue="75" className="slider" id="filterCutoff"/></p>
       </React.Fragment>
     )
   }
