b7f508b81991eaeb0ecb5054cf383fdd7ff0e2c1
Ben Stoller <stollersystemsound@gmail.com>
2020-12-17 12:20:18 -0800

    add transport var and repeat method

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -1,48 +1,53 @@
 import React, {Component} from 'react';
 import * as Tone from 'tone';
-import Controls from './Controls'
+import Controls from './Controls';
+import Heptagram from './Heptagram';
 
 
 class Synth extends Component {
   state = {
-    steps: ["D3","F3","A3","C4","D4","E4","G4","A4"],
-    BPM: 90
+    steps: ["D3","F3","A3","C4","D4","E4","G4"],
+    
   }
-
+  transport = Tone.Transport;
   volume = new Tone.Volume(-20);
   delay = new Tone.FeedbackDelay(.5, .5);  
-  filter = new Tone.Filter(7500, 'lowpass', -24)
+  filter = new Tone.Filter(7500, 'lowpass', -24);
   synth = new Tone.Synth().chain(this.volume, this.delay, this.filter, Tone.Destination);
-
+  index = 0;
   
 
-  notes = this.state.steps
-  sequence = new Tone.Pattern(
-    (time, note) => {
-      this.synth.triggerAttackRelease(note, "8n", time)
-    },
-    this.notes,
-    "upDown"   
-  )
+  // notes = this.state.steps
+  // sequence = new Tone.Pattern(
+  //   (time, note) => {
+  //     this.synth.triggerAttackRelease(note, "8n", time)
+  //   },
+  //   this.notes,
+  //   "upDown"   
+  // )
 
   handleSequenceStart = () => {
     Tone.start();
-    this.sequence.start(0)
-    Tone.Transport.toggle() 
-    Tone.Transport.bpm.value = this.state.BPM  
+    console.log(this.transport.bpm.value)
+    // this.sequence.start(0)
+    this.transport.start()     
   }
 
-  handleSequenceStop = () => {
-    this.sequence.stop()
-    Tone.Transport.toggle()
+  handleSequenceStop = () => {    
+    this.transport.stop()
   }
 
-  handleBPMChange = (bpm) => {
-    this.setState( {BPM: bpm})
+  repeat = (time) => {
+    let stepCount = this.index % 7;    
+    this.synth.triggerAttackRelease(this.state.steps[stepCount], "32n", time)
+    this.index++
   }
+  
 
   componentDidMount() {
 
+    this.transport.bpm.value = 90
+    this.transport.scheduleRepeat(this.repeat, '8n');
     
     var delaySlide = document.getElementById('delayLevel');
     const delay = this.delay; 
@@ -57,10 +62,9 @@ class Synth extends Component {
       filter.frequency.value = this.value*100;     
     });
     
-    var bpmSlide = document.getElementById('bpmCount');
-    const handleBPMchange = this.handleBPMChange
-    bpmSlide.addEventListener("input", function() {     
-      handleBPMchange(this.value);    
+    var bpmSlide = document.getElementById('bpmCount');    
+    bpmSlide.addEventListener("input", function() {         
+      Tone.Transport.bpm.value = this.value
     });
 
   }
@@ -72,7 +76,8 @@ class Synth extends Component {
         <Controls
         start={this.handleSequenceStart}
         stop={this.handleSequenceStop}
-        />        
+        /> 
+        <Heptagram/>
       </React.Fragment>
     )
   }
