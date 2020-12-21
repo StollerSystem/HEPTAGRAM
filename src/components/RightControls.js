cad6abf73215843fcd467bb1322e0a6815fecad1
Ben Stoller <stollersystemsound@gmail.com>
2020-12-21 15:28:08 -0800

    add css fade to controls

--- src/components/RightControls.js
+++ src/components/RightControls.js
@@ -6,7 +6,7 @@ function RightControls() {
   return (
     <div className="controlBox">
       <div className="synthControls" id="synthControl1">
-        <p>SYNTH 1</p>
+        <p>BORDER SYNTH </p>
         <p>OCTAVE<input type="range" min="1" max="5" defaultValue="3" className="slider" id="octave1" /></p>
         <p>VOLUME<input type="range" min="0" max="35" defaultValue="18" className="slider" id="volume1" /></p>
         <p>FILTER<input type="range" min="0" max="100" defaultValue="50" className="slider" id="filterCutoff1" /></p>
@@ -14,7 +14,7 @@ function RightControls() {
         <p>PATTERN<input type="range" min="1" max="4" defaultValue="1" className="slider" id="pattern1" /></p>
       </div>
       <div className="synthControls" id="synthControl2">
-        <p>SYNTH 2</p>
+        <p>STAR SYNTH</p>
         <p>OCTAVE<input type="range" min="1" max="5" defaultValue="2" className="slider" id="octave2" /></p>
         <p>VOLUME<input type="range" min="0" max="35" defaultValue="18" className="slider" id="volume2" /></p>
         <p>FILTER<input type="range" min="0" max="100" defaultValue="50" className="slider" id="filterCutoff2" /></p>
