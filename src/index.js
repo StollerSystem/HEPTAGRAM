35e4a7d80bb97bc2fc0be631b8ae6dfc6adf0117
Ben Stoller <stollersystemsound@gmail.com>
2020-12-22 16:01:49 -0800

    change logo font

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 import App from './components/App'
+import './fonts/digital-7.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'))
